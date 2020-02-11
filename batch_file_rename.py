d10ae17b9aa4e29ac03cec02d809df732a25eac4
stableMan <18010166754@163.com>
2020-02-11 12:11:03 +0800

    batch_file_rename.py done

--- batch_file_rename.py
+++ batch_file_rename.py
@@ -40,10 +40,21 @@ def batch_rename(work_dir, old_ext, new_ext):
 
 def get_parser():
     parser = argparse.ArgumentParser(description='change extension of files in a working directory')
+    # 旧代码
     parser.add_argument('work_dir', metavar='WORK_DIR', type=str, nargs=1,
                         help='the directory where to change extension')
     parser.add_argument('old_ext', metavar='OLD_EXT', type=str, nargs=1, help='old extension')
     parser.add_argument('new_ext', metavar='NEW_EXT', type=str, nargs=1, help='new extension')
+    #  修复   error: the following arguments are required: WORK_DIR, OLD_EXT, NEW_EXT
+    # parser.add_argument('--work_dir', metavar='WORK_DIR', type=str, nargs=1, help='the directory where to change extension')
+    # parser.add_argument('--old_ext', metavar='OLD_EXT', type=str, nargs=1, help='old extension')
+    # parser.add_argument('--new_ext', metavar='NEW_EXT', type=str, nargs=1, help='new extension')
+# 测试
+    args = parser.parse_args()
+    print(args.work_dir)
+    print(args.old_ext)
+    print(args.new_ext)
+
     return parser
 
 
@@ -59,6 +70,8 @@ def main():
     work_dir = args['work_dir'][0]
     # Set the variable old_ext with the second argument passed
     old_ext = args['old_ext'][0]
+
+
     if old_ext[0] != '.':
         old_ext = '.' + old_ext
     # Set the variable new_ext with the third argument passed
@@ -66,8 +79,17 @@ def main():
     if new_ext[0] != '.':
         new_ext = '.' + new_ext
 
+    print(work_dir
+          +"         \r\n"+old_ext
+          +"         \r\n"+new_ext
+          )
+
     batch_rename(work_dir, old_ext, new_ext)
 
-
+# argparse 模块可以让人轻松编写用户友好的命令行接口。
+# 只能通过  命令行执行  命令
+# 【python 所在dir\batch_file_rename.py  work_dir old_ext new_ext 】
+# 案例：把 要替换后缀文件所在dir txt 换成 doc：
+# 【python 所在dir\batch_file_rename.py  要替换后缀文件所在dir txt doc 】
 if __name__ == '__main__':
     main()
